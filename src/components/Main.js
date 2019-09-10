6d961fc84596d06597cdb8e12c22703b75a7f305
Ruben <ruben@pinwi.co>
2019-09-10 15:45:19 +1000

    Main Class - Card placeholder

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Card, Container, Grid, Icon, Image } from "semantic-ui-react";
 
 const Main = () => {
-    return (
-        <h1>Hi</h1>
-    )
-}
+  return (
+    <Grid verticalAlign="middle" centered className="main-container">
+      <Grid.Row>
+        <Grid.Column>
+          <Container className="start">
+            <Card>
+              <Image
+                src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+                wrapped
+                ui={false}
+              />
+              <Card.Content>
+                <Card.Header>Matthew</Card.Header>
+                <Card.Meta>
+                  <span className="date">Joined in 2015</span>
+                </Card.Meta>
+                <Card.Description>
+                  Matthew is a musician living in Nashville.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name="user" />
+                  22 Friends
+                </a>
+              </Card.Content>
+            </Card>
+          </Container>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+};
 
 export default Main;
